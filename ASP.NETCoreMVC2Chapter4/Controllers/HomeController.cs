d819eca066b614ebf1289bf00febe4ae51defd15
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-02-17 20:16:39 +0700

    Реализация расширяющих методов для классов, реализовывающих интерфейс IEnumerable, реализация интерфейса IEnumerable, создание фильтрующих расширяемых методов.

--- ASP.NETCoreMVC2Chapter4/Controllers/HomeController.cs
+++ ASP.NETCoreMVC2Chapter4/Controllers/HomeController.cs
@@ -15,8 +15,18 @@ namespace ASP.NETCoreMVC2Chapter4.Controllers
             {
                 Products = Product.GetProducts()
             };
+            Product[] productArray = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price= 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
             decimal cartPrice = shoppingCart.TotalPrices();
-            return View("Index", new string[] { $"Total: {cartPrice}"});
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
+            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+            return View("Index", new string[] {
+                $"Array Total: {arrayTotal}",
+                $"Filter Total: {nameFilterTotal}"});
         }
     }
 }
