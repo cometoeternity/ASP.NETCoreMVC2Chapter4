28755acbe0554fb972d05547a854fd4c95652941
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-02-17 18:33:23 +0700

    Создание Emrty-проекта, подключение MVC и изменение маршрутизации для ASP.NET Core 3.1

--- ASP.NETCoreMVC2Chapter4/Startup.cs
+++ ASP.NETCoreMVC2Chapter4/Startup.cs
@@ -16,6 +16,7 @@ namespace ASP.NETCoreMVC2Chapter4
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +31,13 @@ namespace ASP.NETCoreMVC2Chapter4
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute (
+                    name: "default",
+                    pattern:"{controller=Home}/{action=Index}/{id?" +
+                    "}"
+                    ) ;
             });
+            
         }
     }
 }
