730c11a3e35dce9a100eaf2a064140f0525e69c1
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-02-17 21:25:05 +0700

    Создание класса с асинхронным методом для запрашивания количества символов и стороннего сайта и асинхронный вызов этой функции в контроллере.

--- ASP.NETCoreMVC2Chapter4/Controllers/HomeController.cs
+++ ASP.NETCoreMVC2Chapter4/Controllers/HomeController.cs
@@ -9,16 +9,10 @@ namespace ASP.NETCoreMVC2Chapter4.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public async Task<ViewResult> Index ()
         {
-            var products = new[]
-            {
-                new {Name="Kayak", Price = 48.95M},
-                new {Name = "Lifejacket", Price = 19.50M},
-                new {Name = "Soccer ball", Price = 34.95M}
-            };
-
-            return View(products.Select(p=> p.Name));
+            long? length = await AsyncMethods.GetPageLength();
+            return View(new string[] { $"Length : {length}" });
         }
     }
 }
---
+++ ASP.NETCoreMVC2Chapter4/Models/AsyncMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ASP.NETCoreMVC2Chapter4.Models
+{
+    public class AsyncMethods
+    {
+        public  async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpMessage= await client.GetAsync("http://apress.com");
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
